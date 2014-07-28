8d7960ee4f2d05e787b0eb8ec2143f896a070379
Mohammad Asjad <admin@Mac-Admin-2.local>
2014-07-28 17:12:38 +0500

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Mohammad Asjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Mohammad Asjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
