56bcfad19e863c5edd491c4c01d1a2b56e35254c
masjad <admin@Mac-Admin-2.local>
2014-07-28 17:28:46 +0500

    added a factory class

---
+++ Git Test/Factory.h
@@ -0,0 +1,13 @@
+//
+//  Factory.h
+//  Git Test
+//
+//  Created by masjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Factory : NSObject
+
+@end
---
+++ Git Test/Factory.m
@@ -0,0 +1,13 @@
+//
+//  Factory.m
+//  Git Test
+//
+//  Created by masjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import "Factory.h"
+
+@implementation Factory
+
+@end
