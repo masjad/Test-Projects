5ee904900159c1aef04eacef97c181bff1aacd39
Mohammad Asjad <admin@Mac-Admin-2.local>
2014-07-28 16:50:58 +0500

    Initial Commit

---
+++ Git Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Mohammad Asjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Mohammad Asjad on 28/07/2014.
+//  Copyright (c) 2014 Mohammad Asjad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
